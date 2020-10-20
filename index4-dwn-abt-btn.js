9c3a7fae9734acd31a45d2f8b73cdad1424a64d1
laraaneke <laraaneke@hotmail.com>
2020-10-20 13:38:51 +0100

    added folio pages to main

--- index4-dwn-abt-btn.js
+++ index4-dwn-abt-btn.js
@@ -10,6 +10,40 @@ const aboutContainer = document.querySelector(".about__container");
 
 const sidebarLink = document.querySelectorAll(".sidebar__item");
 
+const plotterBtns = document.querySelectorAll(".plotter-btn");
+const weavingBackBtn = document.querySelector(".weave-back-btn");
+const weavingCont = document.querySelector(".extended-weaving");
+const weavingBtns = document.querySelectorAll(".weaving-btn");
+
+const plotterCont = document.querySelector(".extended-plotter");
+const plotterBackBtn = document.querySelector(".plotter-back-btn");
+
+
+weavingBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        weavingCont.style.display = 'block'
+        galleryCont.style.display = 'none';
+
+
+    })
+})
+weavingBackBtn.addEventListener("click", e => {
+    weavingCont.style.display = 'none'
+    galleryCont.style.display = 'block';
+
+})
+
+plotterBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        plotterCont.style.display = 'block'
+        galleryCont.style.display = 'none';
+    })
+})
+plotterBackBtn.addEventListener("click", e => {
+    plotterCont.style.display = 'none'
+    galleryCont.style.display = 'block';
+
+})
 
 // home
 sidebarLink[0].addEventListener("click", e => {
@@ -67,7 +101,7 @@ aboutBtn.addEventListener("click", e => {
         aboutContainer.style.display = "block";
 
         introCont.style.display = "none";
-        galleryCont.style.display = "none"
+        galleryCont.style.display = "none";
     }
 
 
